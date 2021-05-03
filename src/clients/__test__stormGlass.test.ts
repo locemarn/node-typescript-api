3e2217898a0a1fef0675b0e32b42254ec1fb629a
marcelo_nogueira <mnsilva@band.com.br>
2021-05-02 22:49:57 -0300

    typing axiosMock on test

--- src/clients/__test__stormGlass.test.ts
+++ src/clients/__test__stormGlass.test.ts
@@ -8,10 +8,15 @@ jest.mock('axios')
 
 describe('StormGlass client', () => {
   it('should return the normalized forecast from the StormGlass service', async() => {
+    const mockedAxios = axios as jest.Mocked<typeof axios>
+
     const lat = -33.792726;
     const lng = 151.289824;
 
-    axios.get = jest.fn().mockResolvedValue({data: stormGlassWeather3HoursFixture})
+    mockedAxios.get
+      .mockResolvedValue({
+        data: stormGlassWeather3HoursFixture
+      })
 
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
